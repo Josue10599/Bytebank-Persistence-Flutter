b2f5ce03b1efa2397389918413648e8c62f452dd
Josue Lopes <josue10599@gmail.com>
2020-05-06 10:32:03 -0300

    Implementada lista de contato

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bytebank_alura/screens/list_contacts.dart';
 
 class DashBoard extends StatelessWidget {
   @override
@@ -12,26 +13,33 @@ class DashBoard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Image.asset('images/bytebank_logo.png'),
-            Container(
-              padding: EdgeInsets.all(8),
-              height: 100,
-              width: 150,
+            Material(
               color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24,
+              child: InkWell(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ListContacts()),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  height: 100,
+                  width: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      Text('CONTACTS',
+                          style: Theme.of(context)
+                              .textTheme
+                              .button
+                              .copyWith(color: Colors.white)),
+                    ],
                   ),
-                  Text('CONTACTS',
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: Colors.white)),
-                ],
+                ),
               ),
             )
           ],
--- lib/screens/list_contacts.dart
+++ lib/screens/list_contacts.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ListContacts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('Nome'),
+              subtitle: Text('Valor'),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
